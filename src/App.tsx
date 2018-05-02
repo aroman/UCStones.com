05daca57a5f65b7f034c202b2881291862352c41
Avi Romanoff <avi@romanoff.me>
2018-05-02 00:51:49 -0400

    functionality MVP done, responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import * as _ from "lodash";
 import * as React from "react";
 import "./App.css";
 
+import {
+  toolRate,
+  toolCost,
+  liberatorCost,
+  liberatorRate,
+  toolNames,
+  liberatorNames
+} from "./GameMechanics";
+
 import Logo from "./logo.png";
 import Plus from "./plus.svg";
 import Trophy from "./trophy.svg";
@@ -13,39 +22,32 @@ enum StoneStatus {
   Missing
 }
 
+const numberWithCommas = (x: number) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
 const GARDEN_SIZE = 5;
 const NUM_STONES = GARDEN_SIZE * 2 + (GARDEN_SIZE - 2) * GARDEN_SIZE;
 const garden = _.times(NUM_STONES).map(i => {
-  const R = Math.random();
-  if (R > 0.66) {
-    return StoneStatus.Free;
-  }
-  if (R > 0.33) {
-    return StoneStatus.Jailed;
-  }
-  return StoneStatus.Missing;
+  // const R = Math.random();
+  return StoneStatus.Jailed;
+  // if (R > 0.66) {
+  //   return StoneStatus.Free;
+  // }
+  // if (R > 0.33) {
+  // }
+  // return StoneStatus.Missing;
 });
 
-const Stone = (props: { status: StoneStatus }) => {
+const Stone = (props: { status: StoneStatus; onFree: () => void }) => {
   if (props.status === StoneStatus.Free) {
     return <div className="Stone StoneFree" />;
   } else if (props.status === StoneStatus.Jailed) {
-    return <div className="Stone StoneJailed" />;
+    return <div className="Stone StoneJailed" onClick={props.onFree} />;
   } else {
     return <div className="Stone" />;
   }
 };
-
-interface PanelProps {
-  title: string;
-  rate: number;
-  unit: string;
-  label: string;
-  quantity: number;
-  cost: number;
-  buttonColor: string;
-  canUpgrade: boolean;
-}
 
 const topScorers = [
   {
@@ -70,28 +72,47 @@ const topScorers = [
   }
 ];
 
+interface PanelProps {
+  title: string;
+  rate: number;
+  unit: string;
+  label: string;
+  quantity: number;
+  cost: number;
+  buttonColor: string;
+  canUpgrade: boolean;
+  canBuy: boolean;
+  onBuy: () => void;
+  onUpgrade: () => void;
+}
+
 const GamePanel = (props: PanelProps) => {
   return (
     <div className="GamePanel">
       <div className="GamePanel-header">
-        <div className="GamePanel-header-title">{props.title}:</div>
+        <div className="GamePanel-header-title">{props.title}</div>
         <div className="GamePanel-header-stats">
-          {props.rate} {props.unit}
+          {numberWithCommas(props.rate)} {props.unit}
         </div>
       </div>
       <div className="GamePanel-inner">
         <div
-          className="GamePanel-button"
+          className={`GamePanel-button ${
+            props.canBuy ? "" : "GamePanel-button-disabled"
+          }`}
           style={{ backgroundColor: props.buttonColor }}
         >
-          <div className="GamePanel-button-inner">
+          <div
+            className="GamePanel-button-inner"
+            onClick={props.canBuy ? props.onBuy : void 0}
+          >
             <img className="GamePanel-button-icon" src={Plus} />
             <div className="GamePanel-button-label">
-              {props.label} ({props.quantity})
+              {props.label} ({numberWithCommas(props.quantity)})
             </div>
           </div>
           <div className="GamePanel-button-subtitle">
-            Cost: {props.cost} stones
+            Cost: {numberWithCommas(props.cost)} stones
           </div>
         </div>
 
@@ -99,6 +120,7 @@ const GamePanel = (props: PanelProps) => {
           <div
             className="GamePanel-button"
             style={{ backgroundColor: "black" }}
+            onClick={props.onUpgrade}
           >
             <div className="GamePanel-button-inner noNudge">
               <img className="GamePanel-button-icon" src={Upgrade} />
@@ -111,7 +133,89 @@ const GamePanel = (props: PanelProps) => {
   );
 };
 
-class App extends React.Component {
+interface AppState {
+  stonesFreed: number;
+  liberatorCount: number;
+  liberatorLevel: number;
+  toolCount: number;
+  toolLevel: number;
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      stonesFreed: 0,
+      toolCount: 1,
+      toolLevel: 0,
+      liberatorLevel: 0,
+      liberatorCount: 0
+    };
+    this.onFree = this.onFree.bind(this);
+    this.buyTool = this.buyTool.bind(this);
+    this.buyLiberator = this.buyLiberator.bind(this);
+    this.upgradeLiberator = this.upgradeLiberator.bind(this);
+    this.upgradeTool = this.upgradeTool.bind(this);
+    setInterval(this.onLiberate.bind(this), 100);
+  }
+
+  get toolCost() {
+    return toolCost(this.state.toolLevel);
+  }
+
+  get liberatorCost() {
+    return liberatorCost(this.state.liberatorLevel);
+  }
+
+  get liberatorRate() {
+    return liberatorRate(this.state.liberatorLevel);
+  }
+
+  get toolRate() {
+    return toolRate(this.state.toolLevel);
+  }
+
+  public onLiberate() {
+    const amount = this.liberatorRate * this.state.liberatorCount;
+    this.setState({
+      stonesFreed: this.state.stonesFreed + amount / 10
+    });
+  }
+
+  public upgradeTool() {
+    this.setState({
+      toolLevel: this.state.toolLevel + 1,
+      toolCount: 1
+    });
+  }
+
+  public upgradeLiberator() {
+    this.setState({
+      liberatorLevel: this.state.liberatorLevel + 1,
+      liberatorCount: 0
+    });
+  }
+
+  public onFree() {
+    this.setState({
+      stonesFreed: this.state.stonesFreed + this.state.toolCount * this.toolRate
+    });
+  }
+
+  public buyTool() {
+    this.setState({
+      stonesFreed: this.state.stonesFreed - this.toolCost,
+      toolCount: this.state.toolCount + 1
+    });
+  }
+
+  public buyLiberator() {
+    this.setState({
+      stonesFreed: this.state.stonesFreed - this.liberatorCost,
+      liberatorCount: this.state.liberatorCount + 1
+    });
+  }
+
   public render() {
     return (
       <div className="App">
@@ -123,7 +227,7 @@ class App extends React.Component {
             </div>
             <div className="BannerCard-instructions">
               <div>1. Tap a UC Stone to free it</div>
-              <div>2. Buy upgrades</div>
+              <div>2. Buy tools and hire liberators</div>
               <div>3. FREE ALL THE STONES!!!</div>
             </div>
           </div>
@@ -131,36 +235,58 @@ class App extends React.Component {
 
         <div className="Game">
           <div className="GameScore">
-            Stones freed:<div className="GameScore-value">341,344</div>
+            Stones freed:<div className="GameScore-value">
+              {numberWithCommas(Math.round(this.state.stonesFreed))}
+            </div>
           </div>
 
-          <GamePanel
-            title="Liberators"
-            unit="stones/sec"
-            rate={132}
-            quantity={43}
-            cost={100}
-            label="112 TA"
-            buttonColor="#d8720f"
-            canUpgrade={true}
-          />
-          <GamePanel
-            title="Tools"
-            unit="stones/tap"
-            rate={132}
-            quantity={43}
-            cost={100}
-            label="David Tepper"
-            buttonColor="#8D19C6"
-            canUpgrade={false}
-          />
+          <div className="GamePanels">
+            <GamePanel
+              title="Tools"
+              unit="stones/tap"
+              rate={this.toolRate}
+              canBuy={this.state.stonesFreed >= this.toolCost}
+              quantity={this.state.toolCount}
+              cost={this.toolCost}
+              label={toolNames[this.state.toolLevel]}
+              buttonColor="#8D19C6"
+              onBuy={this.buyTool}
+              canUpgrade={
+                this.state.stonesFreed >= toolCost(this.state.toolLevel + 1)
+              }
+              onUpgrade={this.upgradeTool}
+            />
+
+            <GamePanel
+              title="Liberators"
+              unit="stones/sec"
+              rate={this.liberatorRate * this.state.liberatorCount}
+              canBuy={this.state.stonesFreed >= this.liberatorCost}
+              quantity={this.state.liberatorCount}
+              cost={this.liberatorCost}
+              label={liberatorNames[this.state.liberatorLevel]}
+              buttonColor="#d8720f"
+              onBuy={this.buyLiberator}
+              canUpgrade={
+                this.state.stonesFreed >=
+                liberatorCost(this.state.liberatorLevel + 1)
+              }
+              onUpgrade={this.upgradeLiberator}
+            />
+          </div>
 
           <div className="Garden">
-            <div className="Garden-logo">UC Stones.com</div>
+            {/* <div className="Garden-logo">UC Stones.com</div> */}
             {_.times(GARDEN_SIZE).map(i => (
               <div className="Garden-row">
                 {_.times(i % (GARDEN_SIZE - 1) === 0 ? GARDEN_SIZE : 2).map(
-                  j => <Stone key={`${i}-${j}}`} status={garden[i * j]} />
+                  j => (
+                    <Stone
+                      onFree={this.onFree}
+                      key={`${i}-${j}}`}
+                      status={garden[i * j]}
+                    />
+                  )
                 )}
               </div>
             ))}
---
+++ src/GameMechanics.ts
@@ -0,0 +1,60 @@
+export const liberatorNames = [
+  "112 TA",
+  "UC Stone Stuco",
+  "UC Stoner",
+  "CMU Qatar",
+  "Stonybrook",
+  "My Little Stony",
+  "12-201",
+  "David Tepper",
+  "Subra Suresh",
+  "Super Suresh",
+  "Super Dupra Suresh"
+];
+
+export const toolNames = [
+  "Plastic knife",
+  "Butter knife",
+  "Safety Scissors",
+  "Lawn mower",
+  "Magnets?",
+  "Scythe",
+  "Nice big axe",
+  "Wire cutters",
+  "Lightsaber",
+  "Telekinesis",
+  "Subronium"
+];
+
+function factorial(n: number) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+export function indexForItem(itemName: string, items: string[]) {
+  const index = items.indexOf(itemName);
+
+  if (index === -1) {
+    throw new Error(`No such item: "${itemName}"`);
+  }
+  return index;
+}
+
+export function liberatorCost(i: number) {
+  return 10 ** (i + 1);
+}
+
+export function liberatorRate(i: number) {
+  return 2 * factorial(i + 1);
+}
+
+export function toolCost(i: number) {
+  return 10 ** (i + 1);
+}
+
+export function toolRate(i: number) {
+  return i === 0 ? 1 : 2 * factorial(i + 1);
+}
